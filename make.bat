1e9f35b945622ff5ad792827362d9bd552d51683
Lars Behl <lars.behl@student.hs-rm.de>
2019-10-30 22:48:38 +0100

    fixed wrong package name in make.bat

--- make.bat
+++ make.bat
@@ -1,1 +1,1 @@
-java -jar "C:\Program Files\Antlr\antlr-4.7.2-complete.jar" -Dlanguage=CSharp LParser.g4 -no-listener -visitor  -package vorlesung %*
+java -jar "C:\Program Files\Antlr\antlr-4.7.2-complete.jar" -Dlanguage=CSharp LParser.g4 -no-listener -visitor  -package ll %*
