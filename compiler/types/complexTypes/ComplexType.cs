3566b2c45040063bdb7cf654fd1739cc143d7507
Lars Behl <lars.behl@student.hs-rm.de>
2019-11-05 23:21:14 +0100

    added type classes

---
+++ compiler/types/complexTypes/ComplexType.cs
@@ -0,0 +1,10 @@
+namespace ll.type
+{
+    public class ComplexType : ll.type.Type
+    {
+        public ComplexType(string typeName) : base(typeName)
+        {
+            
+        }
+    }
+}
---
+++ compiler/types/primitivTypes/BooleanType.cs
@@ -0,0 +1,10 @@
+namespace ll.type
+{
+    public class BooleanType : PrimitivType
+    {
+        public BooleanType() : base("bool")
+        {
+            
+        }
+    }
+}
