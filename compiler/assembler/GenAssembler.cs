2f537216eccc134c6ca310fc66c9560cf6180025
Lars Behl <lars.behl@student.hs-rm.de>
2020-01-18 12:34:57 +0100

    moved makefile to toplevel; azure pipeline should work now

--- compiler/assembler/GenAssembler.cs
+++ compiler/assembler/GenAssembler.cs
@@ -120,9 +120,9 @@ namespace ll.assembler
             string fileName;
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                indexOfSlash = filePath.IndexOf('\\');
+                indexOfSlash = filePath.LastIndexOf('\\');
             else
-                indexOfSlash = filePath.IndexOf('/');
+                indexOfSlash = filePath.LastIndexOf('/');
 
             if (indexOfSlash == -1)
                 fileName = filePath;
@@ -137,7 +137,7 @@ namespace ll.assembler
             if (this.doubleNumbers.Length > 0)
                 fileContent = fileContent + this.doubleNumbers.ToString();
 
-            fileName = fileName.Substring(0, fileName.IndexOf(".ll")) + ".S";
+            fileName = filePath.Substring(0, filePath.IndexOf(".ll")) + ".S";
 
             using (StreamWriter sw = File.CreateText(fileName))
             {
